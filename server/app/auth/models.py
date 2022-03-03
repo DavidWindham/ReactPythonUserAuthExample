28984c5ac4b77bed02a0a05728c92bad2c45219a
DavidWindham <tinymouse2@gmail.com>
2022-03-03 12:42:44 +0000

    Made user and chat columns as nullable false

--- server/app/auth/models.py
+++ server/app/auth/models.py
@@ -8,8 +8,8 @@ from flask import g
 class User(db.Model):
     __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(64), unique=True, index=True)
-    username = db.Column(db.String(64), unique=True)
+    email = db.Column(db.String(64), unique=True, index=True, nullable=False)
+    username = db.Column(db.String(64), unique=True, nullable=False)
     nickname = db.Column(db.String(64))
 
     password_hash = db.Column(db.String(128))
--- server/app/chat/models.py
+++ server/app/chat/models.py
@@ -4,7 +4,7 @@ from .. import db
 class ChatItem(db.Model):
     __tablename__ = 'chat_item'
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))  # Foreign key to user
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)  # Foreign key to user
     user = db.relationship("User")
 
     message = db.Column(db.String(256), nullable=False)
