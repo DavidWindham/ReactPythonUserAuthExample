2c6058ae152626b6c810633b236edd33abb78210
DavidWindham <tinymouse2@gmail.com>
2023-01-04 10:57:36 +0000

    Re-implemented env loading but conditional in case no env

--- client_ts/src/setupProxy.js
+++ client_ts/src/setupProxy.js
@@ -1,31 +1,37 @@
-// const {createProxyMiddleware} = require('http-proxy-middleware')
+const {createProxyMiddleware} = require('http-proxy-middleware')
 
 module.exports = function(app) {
-  // const authProxy = createProxyMiddleware('/auth', {
-  //   target: 'http://flaskjwttest-env.eba-wngtss3f.eu-west-2.elasticbeanstalk.com',
-  //   changeOrigin: true,
-  //   pathRewrite: {
-  //     '^/auth': '',
-  //   },
-  //   logLevel: 'debug',
-  // })
+  if (process.env.REACT_APP_AUTH_PROXY_URL) {
+    const authProxy = createProxyMiddleware('/auth', {
+      target: process.env.REACT_APP_AUTH_PROXY_URL,
+      changeOrigin: true,
+      pathRewrite: {
+        '^/auth': '',
+      },
+      logLevel: 'debug',
+    })
+    app.use(authProxy)
+  }
 
-  // const chatProxy = createProxyMiddleware('/chat', {
-  //   target: 'http://flaskjwttest-env.eba-wngtss3f.eu-west-2.elasticbeanstalk.com',
-  //   changeOrigin: true,
-  //   pathRewrite: {
-  //     '^/chat': '',
-  //   },
-  //   logLevel: 'debug',
-  // })
+  if (process.env.REACT_APP_CHAT_PROXY_URL) {
+    const chatProxy = createProxyMiddleware('/chat', {
+      target: process.env.REACT_APP_CHAT_PROXY_URL,
+      changeOrigin: true,
+      pathRewrite: {
+        '^/chat': '',
+      },
+      logLevel: 'debug',
+    })
+    app.use(chatProxy)
+  }
 
-  // const socketProxy= createProxyMiddleware('/socket.io', {
-  //   target: process.env.REACT_APP_SOCKET_PROXY_URL,
-  //   changeOrigin: true,
-  //   ws: true,
-  //   logLevel: 'debug',
-  // })
-  // app.use(authProxy)
-  // app.use(chatProxy)
-  // app.use(socketProxy)
+  if (process.env.REACT_APP_SOCKET_PROXY_URL) {
+    const socketProxy= createProxyMiddleware('/socket.io', {
+      target: process.env.REACT_APP_SOCKET_PROXY_URL,
+      changeOrigin: true,
+      ws: true,
+      logLevel: 'debug',
+    })
+    app.use(socketProxy)
+  }
 }
