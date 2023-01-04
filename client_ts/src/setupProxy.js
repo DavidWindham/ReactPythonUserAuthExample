15ab782792c012e40ab9bedfa00370f7c5e26510
DavidWindham <tinymouse2@gmail.com>
2023-01-04 10:51:50 +0000

    Removed all proxy, using amplify

--- client_ts/src/setupProxy.js
+++ client_ts/src/setupProxy.js
@@ -1,23 +1,23 @@
-const {createProxyMiddleware} = require('http-proxy-middleware')
+// const {createProxyMiddleware} = require('http-proxy-middleware')
 
 module.exports = function(app) {
-  const authProxy = createProxyMiddleware('/auth', {
-    target: 'http://flaskjwttest-env.eba-wngtss3f.eu-west-2.elasticbeanstalk.com',
-    changeOrigin: true,
-    pathRewrite: {
-      '^/auth': '',
-    },
-    logLevel: 'debug',
-  })
+  // const authProxy = createProxyMiddleware('/auth', {
+  //   target: 'http://flaskjwttest-env.eba-wngtss3f.eu-west-2.elasticbeanstalk.com',
+  //   changeOrigin: true,
+  //   pathRewrite: {
+  //     '^/auth': '',
+  //   },
+  //   logLevel: 'debug',
+  // })
 
-  const chatProxy = createProxyMiddleware('/chat', {
-    target: 'http://flaskjwttest-env.eba-wngtss3f.eu-west-2.elasticbeanstalk.com',
-    changeOrigin: true,
-    pathRewrite: {
-      '^/chat': '',
-    },
-    logLevel: 'debug',
-  })
+  // const chatProxy = createProxyMiddleware('/chat', {
+  //   target: 'http://flaskjwttest-env.eba-wngtss3f.eu-west-2.elasticbeanstalk.com',
+  //   changeOrigin: true,
+  //   pathRewrite: {
+  //     '^/chat': '',
+  //   },
+  //   logLevel: 'debug',
+  // })
 
   // const socketProxy= createProxyMiddleware('/socket.io', {
   //   target: process.env.REACT_APP_SOCKET_PROXY_URL,
@@ -25,7 +25,7 @@ module.exports = function(app) {
   //   ws: true,
   //   logLevel: 'debug',
   // })
-  app.use(authProxy)
-  app.use(chatProxy)
+  // app.use(authProxy)
+  // app.use(chatProxy)
   // app.use(socketProxy)
 }
